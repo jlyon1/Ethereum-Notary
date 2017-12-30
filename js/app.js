50035e5fa2987a930b82ee2dec25382869456292
jlyon1 <joeylyon1@gmail.com>
2017-12-30 11:44:58 -0500

    Update frontend, finalize smart contract

--- js/app.js
+++ js/app.js
@@ -13,8 +13,12 @@ Vue.component("box",{
     <p class="title">Submit a Value</p>
     <p class="subtitle">Store the current time under the current hash/text</p>
     <div class="level is-center">
-    <input class="input" :model="val"></input><button class="button is-danger" @click=triggerSend>Send</button>
+    <input class="input" v-model="val"></input><button class="button is-danger" @click=triggerSend>Send</button>
     </div>
+
+    <div v-if="showStored" class="notification is-success has-text-centered">Stored: {{stored}}</div>
+    <div v-if="showFailed" class="notification is-danger has-text-centered">The Value has already been stored</div>
+
     </div>
     </div>
     <div class="tile is-parent">
@@ -22,21 +26,31 @@ Vue.component("box",{
     <p class="title">Retreive a Value</p>
     <p class="subtitle">See if a date has been stored on the blockchain</p>
     <div class="level is-center">
-    <input class="input" :model="val"></input><button class="button is-danger" @click=triggerSend>Send</button>
+    <input class="input" v-model="checkVal"></input><button class="button is-success" @click=checkHash>Check</button>
     </div>
+
+    <div v-if="showCheck" class="notification is-success has-text-centered">The Date this value was stored was: {{date}}</div>
+
     </div>
     </div>
     </div>
   </div>`,
   data (){
     return {
-      val: "",
+      val: "123456",
+      checkVal: "123456",
+      showStored: false,
+      stored: "",
+      showFailed: false,
+      date: "",
+      showCheck: false,
 
     }
   },
   methods: {
     triggerSend: function(){
       let el = this;
+      date = new Date().getTime();
       web3.eth.getAccounts(function(error,accounts){
         if (error) {
           console.log(error);
@@ -44,11 +58,15 @@ Vue.component("box",{
         var account = accounts[0];
         App.contracts.Notary.deployed().then(function(instance) {
           contractInstance = instance;
-          return contractInstance.storeHash(el.val,(new Date()).getTime(),{from: account});
+          return contractInstance.storeHash(el.val,(date),{from: account});
         }).then(function(val){
-          console.log(val)
+          el.showStored = true;
+          el.stored = new Date(date);
+          el.showFailed = false;
+          el.showStored = true;
         }).catch(function(err) {
-          console.log(err)
+          el.showStored = false;
+          el.showFailed = true;
         });
       });
 
@@ -57,16 +75,15 @@ Vue.component("box",{
       let el = this;
       web3.eth.getAccounts(function(error,accounts){
         if (error) {
-          console.log(error);
         }
         var account = accounts[0];
         App.contracts.Notary.deployed().then(function(instance) {
           contractInstance = instance;
-          return contractInstance.getHashAt(el.val);
+          return contractInstance.getHashAt(el.checkVal);
         }).then(function(val){
-          console.log(val)
+          el.showCheck = true;
+          el.date = new Date(val.c[0])
         }).catch(function(err) {
-          console.log(err)
         });
       });
 
@@ -85,6 +102,7 @@ Vue.component("titlebar",{
   EthNotary</p>
   <p class="subtitle">{{titleText}}</p>
   <p>Store a date on the blockchain using a document hash and retreive it later to prove you owned it in a certain state at a given time. Submitting a hash is a "stamp of ownership", you can also submit any value and so long as no one has stamped it before you can prove ownership.</p>
+  <p>  Something not working right? Open an <a href="https://github.com/jlyon1/ethereum-notary/issues">Issue</a></p>
   </div>
   </div>`,
   data (){
