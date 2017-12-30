a14454fd714bf67a75b069a9cff151156169868b
jlyon1 <joeylyon1@gmail.com>
2017-12-29 19:55:33 -0500

    add message

--- js/app.js
+++ js/app.js
@@ -91,7 +91,7 @@ Vue.component("titlebar",{
     return{
       titleStyle: {position:"absolute",backgroundColor:"#eee",height:"34px",width:"auto",top:"1",left:"0",right:"0"},
       paragraphStyle: {float: "left",height:"34px",lineHeight:"34px",verticalAlign:"center",paddingLeft:"30px",margin:"0"},
-      titleText: "Please Switch to the Ropsten Test Network",
+      titleText: "The contract is not deployed yet, please check back",
       balance: 0,
     }
   },
